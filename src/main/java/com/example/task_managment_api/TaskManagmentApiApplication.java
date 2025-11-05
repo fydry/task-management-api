eec822a027f305fd441cd6707ed00b7ce2d244ef
fydry <dawid.fydryszewski@gmail.com>
2025-11-05 20:13:41 +0100

    Initial project setup

---
+++ src/main/java/com/example/task_managment_api/TaskManagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_managment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_managment_api/TaskManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_managment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
