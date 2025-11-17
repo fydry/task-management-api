a532e0915b9b40aab6464435cd3d6d5b74a6a542
fydry <dawid.fydryszewski@gmail.com>
2025-11-17 20:17:45 +0100

    Update TaskWebController and tasks.html

--- src/main/java/com/example/task_management_api/controller/TaskWebController.java
+++ src/main/java/com/example/task_management_api/controller/TaskWebController.java
@@ -1,12 +1,11 @@
 package com.example.task_management_api.controller;
 
 import com.example.task_management_api.model.Task;
+import com.example.task_management_api.model.TaskStatus;
 import com.example.task_management_api.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TaskWebController {
@@ -30,6 +29,23 @@ public class TaskWebController {
 
         taskService.createTask(task);
 
+        return "redirect:/";
+    }
+
+    @PostMapping("/tasks/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @PostMapping("/tasks/status/{id}")
+    public String updateStatus(@PathVariable Long id, @RequestParam TaskStatus nextStatus) {
+        taskService.getTaskById(id)
+                .map(task -> {
+                    task.setStatus(nextStatus);
+                    return taskService.updateTask(id, task);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found with id" + id));
         return "redirect:/";
     }
 }
